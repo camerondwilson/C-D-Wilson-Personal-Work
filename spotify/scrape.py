7556d26f5c86237edf6ca721a9fe33eb6cd80d67
camerondwilson <cwilson5@berklee.edu>
2021-07-13 12:54:15 -0700

    Spotify Scrape
    Scraped data from Spotify to collect and analyze data for top songs of 2021.

--- spotify/scrape.py
+++ spotify/scrape.py
@@ -4,14 +4,16 @@ import requests
 from time import sleep
 from datetime import date, timedelta
 from selenium import webdriver
+#spotify protection
+#use only bs
 
 url=[]
 dates=[]
-final=[]
+data=[]
 
 #site mapping
 #idk if date in url is needed ex:https://spotifycharts.com/regional/global/daily/2021-07-11
-site = "https://spotifycharts.com/regional/global/daily/"
+site = #request.get"https://spotifycharts.com/regional/global/daily/"+header
 first= date(2020, 7, 11)
 last= date(2021, 7, 11)
 year= last-first
@@ -34,20 +36,30 @@ add_site()
 #song info
 def scrape(y):
     sngl=y
+    #Need to figure out song_date-ex:https://stackoverflow.com/questions/2136267/beautiful-soup-and-extracting-a-div-and-its-contents-by-id
+    #song_date=soup.find("div",{"class": "responsive-select"}).find("date").text
+    #add in position
+    #.split
     for tr in table.find("tbody").findAll("tr"):
-        artist= tr.find("td", {"class": "chart-table-track"}).find("span").text
-        artist= artist.replace("by ","").strip()
-        title= tr.find("td", {"class": "chart-table-track"}).find("strong").text
-        id= tr.find("td", {"class": "chart-table-image"}).find("a").get("href")
-        id= id.split("track/")[1]
-        song_date= y.split("daily/")[1]
+        artist=tr.find("td", {"class": "chart-table-track"}).find("span").text
+        title=tr.find("td", {"class": "chart-table-track"}).find("strong").text
+        id=tr.find("td", {"class": "chart-table-image"}).find("a").get("href")
+        streams=tr.find("td", {"class": "chart-table-streams"}).text
+        position=tr.find("td", {"class": "chart-table-position"}).text
+        song_date=y.split(site)[1]
+        #breakdown by country
+        #use list^
+        #append to url^ 
+        #{} for the f "string{variable(list or single)}"
+        #Scrape position
         
-        final.append([title, artist, id, song_date])
+        data.append([title, artist, song_date, position, streams, id])
 
 #looping
 #need review: https://selenium-python.readthedocs.io/getting-started.html
+#needs a lot of review: https://sites.google.com/chromium.org/driver/
 for pgs in site:
-    driver=webdriver.Chrome()
+    driver=webdriver.Chrome(executable_path=r'C:\Users\camer\OneDrive\Documents\chromedriver')
     driver.get(pgs) 
     read_sngl=requests.get(pgs)
     sleep(5,15)
@@ -56,7 +68,9 @@ for pgs in site:
     scrape(pgs)
  
 #commit and save
-commit = pandas.DataFrame(final, columns= ["Title", "Artist", "Song ID", "Date"])
+#https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+commit=pandas.DataFrame(data, columns= ["Title", "Artist", "Date", "Positon", "Streams", "ID"])
 
+#https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_csv.html
 with open('yearsongs.csv', 'w') as f:
-        commit.to_csv(f, header= True, index=False)
+        commit.to_csv(f, header=True, index=False)
